349f76039baa3b68b7654f9edd5402254de03429
Marcelo V Goncalves <cellobnu@gmail.com>
2015-02-28 13:51:39 -0300

    Resolucao do exercicio parte B

--- module/Market/src/Market/Controller/IndexController.php
+++ module/Market/src/Market/Controller/IndexController.php
@@ -10,12 +10,13 @@
 namespace Market\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return array();
+        return new ViewModel();
     }
 
     public function fooAction()
